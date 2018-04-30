b9831ca643410a009248087bcf392888dd8c0250
Andrew Finke <andrewfinke@me.com>
2018-04-29 19:48:25 -0500

    Init

---
+++ lake-server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const WebSocket = require('ws');
+const SocketServer = WebSocket.Server;
+
+const path = require('path');
+const INDEX = path.join(__dirname, 'index.html');
+
+const PORT = process.env.PORT || 3000;
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.isAlive = true;
+  ws.on('pong', heartbeat);
+
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
+function noop() {}
+
+function heartbeat() {
+  this.isAlive = true;
+}
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping(noop);
+  });
+}, 10000);
